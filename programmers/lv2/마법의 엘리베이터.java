a98c21e5ec6e8608bfe3a579b8ac7a9fb1aa1121
dnjmw8612 <dnjwm8612@gmail.com>
2023-01-18 23:28:55 +0900

    마법의 엘리베이터

---
+++ programmers/lv2/마법의 엘리베이터.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int storey) {
+        int answer = 0;
+        
+        while(storey > 0){
+            int n = storey % 10;
+            
+            if(n >= 6){
+                answer += 10 - n;
+                storey += 10 - n;
+            }else if(n == 5 && (storey / 10) % 10 >= 5){
+                answer += 10 - n;
+                storey += 10 - n;
+            }else{
+                answer += n;
+            }
+            
+            storey /= 10;
+        }
+        
+        return answer;
+    }
+}
